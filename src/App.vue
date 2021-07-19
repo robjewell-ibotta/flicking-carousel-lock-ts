81b85b7b2c297a0919de7093b03c63405a16b8ab
Rob Jewell <rob.jewell@ibotta.com>
2021-07-19 08:57:52 -0400

    banners rotating

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,35 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class='carousel-container'>
+      <Flicking v-if="banners.data.images.length" :options="{ circular: true, renderOnlyVisible: true }">
+        <img
+          v-for="banner in banners.data.images"
+          :key="banner.id"
+          :src="banner.image.url"
+          :alt="banner.image.alt"
+          class="banner-image" />
+      </Flicking>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import mockResponse from '@/components/mockBanners';
 import HelloWorld from './components/HelloWorld.vue';
+import { Flicking } from "@egjs/vue-flicking";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+
+@Component({components: {HelloWorld, Flicking}})
+export default class App extends Vue {
+  banners = mockResponse;
+}
 </script>
 
 <style>
+@import '~@egjs/vue-flicking/dist/flicking.css';
+@import '~@egjs/vue-flicking/dist/flicking-inline.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,5 +38,14 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.carousel-container {
+  margin-bottom: 10px;
+}
+.carousel-container img {
+  width: 100%;
+  border-radius: 9.5px;
+  -webkit-user-drag: none;
 }
 </style>
